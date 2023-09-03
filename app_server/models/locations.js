2130cc7a2f63460ca517522d0b18f8f410c6a997
yashwanthkosuri <yashwanthkosuri29@gmail.com>
2023-09-03 20:06:49 +0530

    Mongoose connected

--- app_server/models/locations.js
+++ app_server/models/locations.js
@@ -1,19 +1,6 @@
 const mongoose = require('mongoose');
 
-const openingTimesSchema = new mongoose.Schema({
-  days: {
-    type: String,
-    required: true
-  },
-  opening: String,
-  closing: String,
-  closed: {
-    type: Boolean,
-    required: true
-  }
-});
-
-const reviewSchema = new mongoose.Schema({
+var reviewSchema = new mongoose.Schema({
   author: String,
   rating: {
     type: Number,
@@ -28,27 +15,15 @@ const reviewSchema = new mongoose.Schema({
   }
 });
 
-const locationSchema = new mongoose.Schema({
-  name: {
+var openingTimesSchema = new mongoose.Schema({
+  days: {
     type: String,
     required: true
   },
-  address: String,
-  rating: {
-    type: Number,
-    'default': 0,
-    min: 1,
-    max: 5
-  },
-  facilities: [String],
-  coords: {
-    type: { type: String},
-    index: [Number]
-  },
-  openingTimes: [openingTimesSchema],
-  reviews: [reviewSchema]
+  opening: String,
+  closing: String,
+  closed: {
+    type: Boolean,
+    required: true
+  }
 });
-
-locationSchema.index({coords: '2dsphere'});
-
-mongoose.model('Location', locationSchema);
