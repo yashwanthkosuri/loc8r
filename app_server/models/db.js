e3b2856409da6bc33e1a14c40b6d4aa48414b6c6
Yashwanth <87197517+yashwanthkosuri@users.noreply.github.com>
2023-09-13 15:36:48 +0530

    Update db.js

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,13 +1,8 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
-const PORT = process.env.PORT || 3000;
 
-var gracefulShutdown;
-var dbURI = "mongodb+srv://22eg506a08:geWQn7KJLNOZUzI3@cluster0.m79efcq.mongodb.net/";
-if (process.env.NODE_ENV === 'production') {
-  dbURI = process.env.MONGODB_URI;
-}
-mongoose.connect(dbURI);
+const dbURI = 'mongodb://localhost/Loc8r';
+
+mongoose.connect(dbURI, {useNewUrlParser: true});
 mongoose.connection.on('connected', () => {
  console.log(`Mongoose connected to ${dbURI}`);
 });
@@ -17,5 +12,29 @@ mongoose.connection.on('error', err => {
 mongoose.connection.on('disconnected', () => {
  console.log('Mongoose disconnected');
 });
+const gracefulShutdown = (msg, callback) => {
+ mongoose.connection.close( () => {
+ console.log(`Mongoose disconnected through ${msg}`);
+ callback();
+ });
+};
+// For nodemon restarts
+process.once('SIGUSR2', () => {
+ gracefulShutdown('nodemon restart', () => {
+ process.kill(process.pid, 'SIGUSR2');
+ });
+});
+ // For app termination
+process.on('SIGINT', () => {
+    gracefulShutdown('app termination', () => {
+    process.exit(0);
+    });
+   });
+   // For Heroku app termination
+   process.on('SIGTERM', () => {
+    gracefulShutdown('Heroku app shutdown', () => {
+    process.exit(0);
+    });
+   });
 
 require('./locations');
