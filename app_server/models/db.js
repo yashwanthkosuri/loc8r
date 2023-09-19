54766c687014e29eefbf1fd98d7de731597e0975
yashwanthkosuri <yashwanthkosuri29@gmail.com>
2023-09-20 00:14:11 +0530

    Mongoose connected

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
-
 const dbURI = 'mongodb://localhost/Loc8r';
-
 mongoose.connect(dbURI, {useNewUrlParser: true});
 mongoose.connection.on('connected', () => {
  console.log(`Mongoose connected to ${dbURI}`);
@@ -24,17 +22,17 @@ process.once('SIGUSR2', () => {
  process.kill(process.pid, 'SIGUSR2');
  });
 });
- // For app termination
+// For app termination
 process.on('SIGINT', () => {
-    gracefulShutdown('app termination', () => {
-    process.exit(0);
-    });
-   });
-   // For Heroku app termination
-   process.on('SIGTERM', () => {
-    gracefulShutdown('Heroku app shutdown', () => {
-    process.exit(0);
-    });
-   });
+ gracefulShutdown('app termination', () => {
+ process.exit(0);
+ });
+});
+// For Heroku app termination
+process.on('SIGTERM', () => {
+ gracefulShutdown('Heroku app shutdown', () => {
+ process.exit(0);
+ });
+});
 
 require('./locations');
