841d549d3111babd7cac4aab7100470e3e9719c0
yashwanthkosuri <yashwanthkosuri29@gmail.com>
2023-09-30 17:00:03 +0530

    Mongoose connected

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,38 +1,48 @@
-const mongoose = require('mongoose');
-const dbURI = 'mongodb://localhost/Loc8r';
-mongoose.connect(dbURI, {useNewUrlParser: true});
-mongoose.connection.on('connected', () => {
- console.log(`Mongoose connected to ${dbURI}`);
+var mongoose = require('mongoose');
+var gracefulShutdown;
+var dbURI = 'mongodb://localhost/Loc8r';
+if (process.env.NODE_ENV === 'production') {
+    dbURI = process.env.MONGOLAB_URI;
+}
+
+mongoose.connect(dbURI);
+
+// CONNECTION EVENTS
+mongoose.connection.on('connected', function() {
+    console.log('Mongoose connected to ' + dbURI);
 });
-mongoose.connection.on('error', err => {
- console.log(`Mongoose connection error: ${err}`);
+mongoose.connection.on('error', function(err) {
+    console.log('Mongoose connection error: ' + err);
 });
-mongoose.connection.on('disconnected', () => {
- console.log('Mongoose disconnected');
+mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose disconnected');
 });
-const gracefulShutdown = (msg, callback) => {
- mongoose.connection.close( () => {
- console.log(`Mongoose disconnected through ${msg}`);
- callback();
- });
+
+// CAPTURE APP TERMINATION / RESTART EVENTS
+// To be called when process is restarted or terminated
+gracefulShutdown = function(msg, callback) {
+    mongoose.connection.close(function() {
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
 };
 // For nodemon restarts
-process.once('SIGUSR2', () => {
- gracefulShutdown('nodemon restart', () => {
- process.kill(process.pid, 'SIGUSR2');
- });
+process.once('SIGUSR2', function() {
+    gracefulShutdown('nodemon restart', function() {
+        process.kill(process.pid, 'SIGUSR2');
+    });
 });
 // For app termination
-process.on('SIGINT', () => {
- gracefulShutdown('app termination', () => {
- process.exit(0);
- });
+process.on('SIGINT', function() {
+    gracefulShutdown('app termination', function() {
+        process.exit(0);
+    });
 });
 // For Heroku app termination
-process.on('SIGTERM', () => {
- gracefulShutdown('Heroku app shutdown', () => {
- process.exit(0);
- });
+process.on('SIGTERM', function() {
+    gracefulShutdown('Heroku app termination', function() {
+        process.exit(0);
+    });
 });
 
 // BRING IN YOUR SCHEMAS & MODELS
